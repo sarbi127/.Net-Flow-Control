4b7017cac70d1f4d18db46069c0bb907302d1c7d
Sara Binazir <sarabinazir@gmail.com>
2019-10-25 04:22:12 -0700

    Add project files.

---
+++ Uppgift2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Uppgift2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                System.Console.WriteLine("Type number and press the Enter Key:");
+                System.Console.WriteLine( "0.  Exit ");
+                System.Console.WriteLine( "1.  Checks whether a person is a pensioner or youth at the specified age.");
+                System.Console.WriteLine( "2.  Checks number of people and total cost for the whole company.");
+                System.Console.WriteLine( "3.  Please write what you want and it is scan 10 times.");
+                System.Console.WriteLine( "4.  Please write at least three words and it is divides into words.");
+                //try
+                //{
+                   int a = int.Parse(System.Console.ReadLine());
+
+                    //int a;
+                    string answer;
+                    int  TCost;
+                    Util util = new Util();
+
+                    switch (a)
+                    {
+                        case 0:
+                            {
+                                return;
+                            }
+                        case 1:
+                            {
+                                Console.WriteLine("Enter your age:");
+                                answer = Console.ReadLine();
+                                util.Person(answer);
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Enter how many we are going to the cinema:");
+                                answer = Console.ReadLine();
+                                TCost = util.TotalCost(answer);
+                                Console.WriteLine( $"Number of people: {answer}, Total cost: {TCost} kr ");
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.WriteLine("Please write what you want");
+                                answer = Console.ReadLine();
+                                util.Scan(answer);
+                                break;
+                            }
+                        case 4:
+                            {
+                                Console.WriteLine("Please write at least 3 words");
+                                answer = Console.ReadLine();
+                                util.Split(answer);
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine(" It is incorrect input ");
+                                break;
+                            }
+                    }
+               // }
+                //catch
+                //{
+                //}
+            }
+        }
+    }
+}
+        
+    
+
---
+++ Uppgift2/Util.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uppgift2
+{
+    class Util
+    {
+        //Checks whether a person is a pensioner or youth at the specified age.
+        public void Person(string input)
+        {
+            int result = Int32.Parse(input);
+
+            if (result < 20)
+            {
+                Console.WriteLine("Youth price: 80kr ");
+
+            }
+            else if (result > 64)
+            {
+                Console.WriteLine("Pensioner price: SEK 90 ");
+            }
+            else
+            {
+                Console.WriteLine("Standard price: SEK 120 ");
+            }
+
+        }
+
+        //Checks number of people and total cost for the whole company. 
+         public int TotalCost(string num)
+        {
+            int result_num = Int32.Parse(num);
+            string age;
+            int result_cost;
+            int sum = 0;
+
+            for (int a = 1; a <= result_num; a = a + 1)
+            {
+                Console.WriteLine($"Enter age for person {a}");
+                age = Console.ReadLine();
+                result_cost = Int32.Parse(age);
+
+                if (result_cost < 20)
+                {
+                    // Console.WriteLine(" Youth price: 80kr ");
+                    sum += 80;
+                }
+                else if (result_cost > 64)
+                {
+                    //Console.WriteLine(" Pensioner price: SEK 90 ");
+                    sum += 90;
+                }
+                else
+                {
+                    //Console.WriteLine(" Standard price: SEK 120 ");
+                    sum += 120;
+                }
+
+            }
+
+            return sum;
+        }
+
+        //write what you want and it is scan 10 times. 
+        public void Scan(string sentenc)
+        {
+            for (int a = 1; a <= 10; a++)
+            {
+                Console.Write($"{a}.{sentenc},");
+            }
+        }
+
+
+        //write what you want and it is scan 10 times. 
+        public void Split(string phrase)
+        {
+            string[] words = phrase.Split(' ');
+            if (words.Length >= 3)
+            {
+                //foreach (var word in words)
+                //{
+                System.Console.WriteLine($"<{words[2]}>");
+                //}
+            }
+            else
+            {
+                System.Console.WriteLine($"Less than three words!");
+            }
+        }
+
+    }
+}
